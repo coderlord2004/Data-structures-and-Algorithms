4f6184c67f1329228e74e9bbe621b740c03804f2
coderlord2004 <duyn38520@gmail.com>
2024-02-03 12:16:04 +0700

    first commit

--- code_ptit.cpp
+++ code_ptit.cpp
@@ -1,48 +1,32 @@
-// MergeSort và đếm số cặp nghịch thế với độ phức tạp O(nlog(n));
-
+/*
+Cho mảng A[] gồm n phần tử. Hãy tìm số phép đổi chỗ ít nhất
+giữa các phần tử của mảng để mảng A[] được sắp xếp. Ví dụ với A[] = {4, 3, 2, 1}
+ta cần thực hiện ít nhất 2 phép đổi chỗ: Swap(A[0], A[3]),  Swap(A[1], A[2]).
+*/
 #include<bits/stdc++.h>
 using namespace std;
-int Merge(int a[], int l, int mid, int r){
-    vector<int> x(a+l, a+mid+1);
-    vector<int> y(a+mid+1, a+r+1);
-    int i = 0, j = 0, dem = 0;
-    while(i<x.size() && j < y.size()){
-        if(x[i] <= y[j]){
-            a[l++] = x[i++];
-        }
-        else {
-            a[l++] = y[j++];
-            dem += (x.size() - i); /*nếu có 1 cặp nghịch thế x[i]>y[j] (i<j) thì tất cả các phần tử
-                                     sau i cũng tạo thành cặp nghịch thế với y[j] nên số cặp nghịch thế bằng x.size()-i;*/
-        }
-    }
-    while (i < x.size())
-    {
-        a[l++] = x[i++];
-    }
-    while (j < y.size())
-    {
-        a[l++] = y[j++];
-    }
-    return dem;
-}
-void mergeSort(int a[], int l, int r){
-    int dem = 0; //khi l=r thì dem = 0;
-    if(l<r){
-        int mid = (l+r)/2;
-        dem += mergeSort(a, l, mid);
-        dem += mergeSort(a, mid+1, r);
-        dem += Merge(a, l, mid, r);
-    }
-    return dem;
-}
+
 int main(){
     int t; cin >> t;
     while(t--){
-        int n;
-        cin >> n;
-        int a[n];
-        for(int &x : a) cin >> x;
-        cout <<"Số cặp nghịch thế: " mergeSort(a, 0, n-1) << endl;
+        int n; cin >> n;
+        int a[n], b[n];
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+            b[i] = a[i];
+        }
+        sort(a, a+n); // so sánh 2 mảng a đã sắp xếp với mảng b chưa sắp xếp và đếm số lần đổi chỗ ít nhất.
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(b[i] != a[i]){ //tìm thấy phần tử đã bị swap.
+                int min_pos = i; // tìm vị trí của phần tử nhỏ nhất và swap với phần tử hiện tại.
+                for(int j = i+1; j<n; j++){
+                    if(b[min_pos] > b[j]) min_pos = j;
+                }
+                swap(b[i], b[min_pos]);
+                ++cnt;
+            }
+       }
+       cout << cnt << endl;
     }
 }
