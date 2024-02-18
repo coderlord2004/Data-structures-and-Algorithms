eb9c206524e10120ea483cf58c6329336370cd18
coderlord2004 <duyn38520@gmail.com>
2024-02-18 23:51:49 +0700

    Bộ ba số có tổng bằng K

--- code_ptit.cpp
+++ code_ptit.cpp
@@ -1,53 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k;
-int x[100], a[100];
-vector<vector<int>> v;
-void Try(int i){
-    for(int j=0; j<=1; j++){
-        x[i] = j;
-        if(i == n){
-            int sum = 0;
-            for(int c=1; c<=n; c++){
-                if(x[c]){
-                    sum += a[c];
-                }
-            }
-            if(sum == k){
-                vector<int> b;
-                for(int c=1; c<=n; c++){
-                    if(x[c]){
-                        b.push_back(a[c]);
-                    }
-                }
-                v.push_back(b);
-            }
-        }
-        else Try(i+1);
-    }
-}
+#define ll long long
 int main(){
     int t; cin >> t;
     while(t--){
-        cin >> n >> k;
-        for(int i=1; i<=n; i++){
-            cin >> a[i];
-        }
-        sort(a+1, a+n+1);
-        Try(1);
-        if(v.empty() == true) cout << -1 << endl;
-        else {
-            sort(v.begin(), v.end());
-            for(int i=0; i<v.size(); i++){
-                cout << "[";
-                for(int j=0; j<v[i].size(); j++){
-                    if(j < v[i].size()-1) cout << v[i][j] << " ";
-                    else cout << v[i][j] << "] ";
-                }
+        int n, k; cin >> n >> k;
+        ll a[n];
+        for(ll &x : a) cin >> x;
+        sort(a, a+n);
+        ll dem = 0;
+        for(int i=0; i<n-2; i++){
+            if(a[i] > k) break;
+            for(int j=i+1; j<n-1; j++){
+                auto key = lower_bound(a+j+1, a+n, k-a[i]-a[j]);
+                dem += (key - (a+j+1));
             }
-            cout << endl;
-            v.clear();
-
         }
+        cout << dem << endl;
     }
 }
