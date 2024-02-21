3238e0e21a1d1145e9994df0f8e8b3b1e41e277e
coderlord2004 <duyn38520@gmail.com>
2024-02-21 17:03:31 +0700

    Cặp số có tổng bằng K

--- DSA06032.cpp
+++ DSA06032.cpp
@@ -1,21 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+int first_pos(int a[], int l, int r, int x){
+    int res = -1;
+    while(l <= r){
+        int m = (l+r)/2;
+        if(a[m] == x){
+            res = m;
+            r = m-1;
+        }
+        else if(a[m] < x) l = m+1;
+        else r = m-1;
+    }
+    return res;
+}
+int last_pos(int a[], int l, int r, int x){
+    int res = -1;
+    while(l <= r){
+        int m = (l+r)/2;
+        if(a[m] == x){
+            res = m;
+            l = m+1;
+        }
+        else if(a[m] < x) l = m+1;
+        else r = m-1;
+    }
+    return res;
+}
 int main(){
     int t; cin >> t;
     while(t--){
         int n, k; cin >> n >> k;
-        ll a[n];
-        for(ll &x : a) cin >> x;
-        sort(a, a+n);
-        ll dem = 0;
-        for(int i=0; i<n-2; i++){
-            if(a[i] > k) break;
-            for(int j=i+1; j<n-1; j++){
-                auto key = lower_bound(a+j+1, a+n, k-a[i]-a[j]);
-                dem += (key - (a+j+1));
-            }
+        int a[n];
+        map<int, int> mp;
+        for(int &x : a) {
+            cin >> x;
+            mp[x]++;
         }
-        cout << dem << endl;
-    }
+        sort(a, a+n);
+        ll cnt = 0;
+        for(int i=0; i<n; i++){
+            int key1 = last_pos(a, i+1, n-1, k-a[i]);
+            int key2 = first_pos(a, i+1, n-1, k-a[i]);
+            if(key1 != -1){
+                cnt += (key1 - key2 + 1); 
+            }
+        }        
+        cout << cnt << endl;
+   }
 }
